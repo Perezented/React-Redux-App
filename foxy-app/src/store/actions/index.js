acd15eec56fd29dd2baf4cc72cff22cae3d04653
Michael Perez <mperez6473@gmail.com>
2020-04-16 01:02:54 -0500

    Reducers look to be done.

--- foxy-app/src/store/actions/index.js
+++ foxy-app/src/store/actions/index.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
-export const FETCH_QUOTE_FAILURE = 'FETCH_QUOTE_FAILURE'
+export const FETCH_IMAGE_FAILURE = 'FETCH_IMAGE_FAILURE'
 
 export const fetchImage = () => {
     return dispatch => {
-        dispatch({type: FETCH_QUOTE_FAILURE})
+        dispatch({type: FETCH_IMAGE_FAILURE})
         axios.get('https://randomfox.ca/floof/')
         .then(response=> {
             console.log(response)
             //dispatch({type: 'FETCH_QUOTE_SUCCESS', payload: res.data....})
         })
         .catch(err => dispatch({
-            type: FETCH_QUOTE_FAILURE,
+            type: FETCH_IMAGE_FAILURE,
             payload: `Error ${err.response.status}: ${err.response.data}`
         }))
     }
---
+++ foxy-app/src/store/reducers/imagesReducer.js
@@ -0,0 +1,30 @@
+const initialState ={
+    img: '',
+    isFetching: false,
+    error: '',
+}
+
+export const imagesReducer = (state=initialState, action) => {
+    switch(action.type){
+        case 'FETCH_IMAGE_START':
+            return {
+                ...state, 
+                isFetching: true
+            }
+            case 'FETCH_IMAGE_SUCCESS': 
+            return {
+                ...state,
+                isFetching: false,
+                image: action.payload,
+                error: ''
+            }
+            case 'FETCH_IMAGE_FAILURE':
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+            default:
+                return state
+    }
+}
