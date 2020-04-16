60bb478cfb012faca970ce301c4c8f902a139903
Michael Perez <mperez6473@gmail.com>
2020-04-16 00:41:55 -0500

    Added a store folder that is holding an actions folder. Added components folder that now has the main container. other dependencies were installed

---
+++ foxy-app/src/components/FoxyImg.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import Loader from 'react-loader-spinner'
+
+
+const FoxyImage = props => {
+    useEffect(()=>{
+        props.fetchImage()
+    }, [])
+}
+export const FoxyImg =()=>{
+    console.log('This is the FoxyImg: ')
+    return (     
+        <h1>Look at all these foxy images!</h1>
+
+        )
+}
