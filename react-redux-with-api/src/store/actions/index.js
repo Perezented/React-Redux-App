f8504cabb681ba533bf2dab0124bceb748d09748
Michael Perez <mperez6473@gmail.com>
2020-05-13 16:13:41 -0500

    Added the different data fetch options in both reducers and actions.

--- react-redux-with-api/src/store/actions/index.js
+++ react-redux-with-api/src/store/actions/index.js
@@ -1,12 +1,27 @@
 import axios from 'axios';
 export const FETCH_DATA_START = 'FETCH_DATA_START';
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
+export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
 
 export const fetchData = () => {
     return (dispatch) => {
         dispatch({ type: FETCH_DATA_START });
 
-        axios.get('https://api.ratesapi.io/api/latest?base=USD').then((res) => {
-            console.log(res.data);
-        });
+        axios
+            .get('https://api.ratesapi.io/api/latest?base=USD')
+            .then((res) => {
+                console.log(res);
+                dispatch({
+                    type: FETCH_DATA_SUCCESS,
+                    payload: res.data,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch({
+                    type: FETCH_DATA_FAILURE,
+                    payload: err.data,
+                });
+            });
     };
 };
--- react-redux-with-api/src/store/reducers/getDataReducer.jsx
+++ react-redux-with-api/src/store/reducers/getDataReducer.jsx
@@ -1,13 +1,30 @@
-import { FETCH_DATA_START } from '../actions';
+import {
+    FETCH_DATA_START,
+    FETCH_DATA_SUCCESS,
+    FETCH_DATA_FAILURE,
+} from '../actions';
 
 const initialState = {
     isFetching: false,
     error: '',
-    dataArray: [],
+    dataArray: null,
 };
 
 export const dataFetchReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_DATA_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false,
+            };
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state,
+                error: '',
+                isFetching: false,
+                dataArray: action.payload,
+            };
         case FETCH_DATA_START:
             return {
                 ...state,
@@ -15,6 +32,7 @@ export const dataFetchReducer = (state = initialState, action) => {
                 error: '',
                 dataArray: action.payload,
             };
+
         default:
             return state;
     }
